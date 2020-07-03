3a45e68b52ceb8bafcb40a325f1b8124d811eb6c
jingw2 <jingwangtju@gmail.com>
2020-07-03 17:27:11 +0800

    Add files via upload

---
+++ tests/test_da.py
@@ -0,0 +1,41 @@
+#!usr/bin/python 3.7
+#-*-coding:utf-8-*-
+
+import pytest 
+import sys 
+import os 
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(path)
+from src import da
+
+class TestDA:
+
+    def test_input(self):
+        with pytest.raises(AssertionError):
+            da.DeterministicAnnealing(n_clusters=2, distribution=[0.25, 0.3])
+        with pytest.raises(AssertionError):
+            da.DeterministicAnnealing(n_clusters=1, distribution=[0.25, 0.3])
+        with pytest.raises(AssertionError):
+            da.DeterministicAnnealing(n_clusters=2, distribution=[0.25, 0.75], T=0.1)
+    
+    def test_output(self):
+        import collections
+        import random 
+        import numpy as np 
+        n_samples = 1000
+        random_state = 42
+        random.seed(random_state)
+        np.random.seed(random_state)
+        X = np.random.rand(n_samples, 2)
+        n_clusters = 4
+        distribution = [0.25] * n_clusters
+
+        model = da.DeterministicAnnealing(n_clusters, distribution)
+        model.fit(X)
+
+        labels = model.labels_
+        label_counter = collections.Counter(labels)
+        label_dist = list(label_counter.values())
+        label_dist = [d / np.sum(label_dist) for d in label_dist]
+
+        assert np.sum(np.array(label_dist) - np.array(distribution)) <= 1e-6
---
+++ tests/test_minmax.py
@@ -0,0 +1,49 @@
+#!usr/bin/python 3.6
+#-*-coding:utf-8-*-
+
+import pytest 
+import sys 
+import os 
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(path)
+from src import minmax
+
+class TestMinMax:
+
+    def test_input(self):
+        with pytest.raises(AssertionError):
+            minmax.MinMaxKMeansMinCostFlow(n_clusters=-1, size_min=1, size_max=2)
+        with pytest.raises(AssertionError):
+            minmax.MinMaxKMeansMinCostFlow(n_clusters=0, size_min=1, size_max=2)
+        with pytest.raises(AssertionError):
+            minmax.MinMaxKMeansMinCostFlow(n_clusters=1, max_iters=1.2, size_min=1, size_max=2)
+        with pytest.raises(Exception):
+            minmax.MinMaxKMeansMinCostFlow(n_clusters=1, distance_func="a", size_min=1, size_max=2)
+        with pytest.raises(AssertionError):
+            minmax.MinMaxKMeansMinCostFlow(n_clusters=1, size_min=None, size_max=2)
+        with pytest.raises(AssertionError):
+            minmax.MinMaxKMeansMinCostFlow(n_clusters=1, size_min=-1, size_max=2)
+        with pytest.raises(AssertionError):
+            minmax.MinMaxKMeansMinCostFlow(n_clusters=1, size_min=20, size_max=10)
+    
+    def test_output(self):
+        from sklearn.datasets import make_blobs
+        import collections 
+
+        n_samples = 2000
+        n_clusters = 4  # use 3 bins for calibration_curve as we have 3 clusters here
+        centers = [(-5, -5), (0, 0), (5, 5), (7, 10)]
+
+        X, _ = make_blobs(n_samples=n_samples, n_features=2, cluster_std=1.0,
+                        centers=centers, shuffle=False, random_state=42)
+        
+        minsize = 200
+        maxsize = 800
+        model = minmax.MinMaxKMeansMinCostFlow(n_clusters, size_min=minsize, 
+            size_max=maxsize)
+        model.fit(X)
+
+        label_counter = collections.Counter(model.labels_)
+
+        for label, count in label_counter.items():
+            assert count >= minsize and count <= maxsize
